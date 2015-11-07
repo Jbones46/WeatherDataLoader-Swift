0438a5127655139aa4f96edaca0de85f09e41cc8
Justin Ferre <Jman4647@gmail.com>
2015-11-07 09:12:01 -0500

    Initial Commit

---
+++ WeatherDataLoader/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WeatherDataLoader
+//
+//  Created by Justin Ferre on 11/7/15.
+//  Copyright © 2015 Justin Ferre. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WeatherDataLoader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WeatherDataLoader
+//
+//  Created by Justin Ferre on 11/7/15.
+//  Copyright © 2015 Justin Ferre. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
